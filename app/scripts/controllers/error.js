9e0ac6afebf9d5958d1eb858ed51cb8e44a30aed
Vineeth Jose <vjose@equilar.com>
2016-05-19 16:27:32 -0700

    Redirect to login page if jwt is missing.

--- app/scripts/controllers/error.js
+++ app/scripts/controllers/error.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('stormpathIdpApp')
-  .controller('ErrorCtrl', function ($scope,Stormpath) {
+  .controller('ErrorCtrl', function ($scope,$location,Stormpath) {
     $scope.errors = Stormpath.errors;
     $scope.inError = false;
     $scope.$watchCollection('errors',function(){
       $scope.inError = $scope.errors.length > 0;
+      if($scope.inError) {
+    	  if($location.search().jwt == null) {
+    		  window.location.href = 'http://www.equilar.com/login.html';
+    	  }
+      }
     });
   });
