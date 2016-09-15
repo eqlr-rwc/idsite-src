ceb958fe9e1cb406238b8a8db576276ab6760dcb
Chen Yang <cyang@equilar.com>
2016-09-15 15:35:04 -0700

    clean o2p

--- app/scripts/controllers/components.js
+++ app/scripts/controllers/components.js
@@ -1,14 +1,14 @@
 
 	var componentInfo = [];
  // componentInfo[ 'HASH'] = ['NAME', 'URL', 'LOGO', 'INFO_FRAME' ],
-    componentInfo['3luD8Rvh'] = ['BEUA QA', 'https://qa-beua.equilar.com', '', ''];
-    componentInfo['46yW5Us4'] = ['BEUA STAGE', 'https://stage-beua.equilar.com', '', ''];
+    componentInfo['3luD8Rvh'] = ['BEUA QA', 'https://qa-beua-o2p.equilar.com', '', ''];
+    componentInfo['46yW5Us4'] = ['BEUA STAGE', 'https://stage-o2p-beua.equilar.com', '', ''];
     componentInfo['4NcuZtp9'] = ['BEUA PROD', 'https://beua.equilar.com', '', ''];
     componentInfo['4aT6Puas'] = ['BoardEdge QA', 'https://qa-aws-bsp.equilar.com', 'images/logo-boardedge.png', 'boardedge-frame'];
     componentInfo['5741xJ1r'] = ['BoardEdge STAGE', 'https://stage-boardedge.equilar.com', 'images/logo-boardedge.png', 'boardedge-frame'];
     componentInfo['2oq3Aswp'] = ['BoardEdge PROD', 'https://boardedge.equilar.com', 'images/logo-boardedge.png', 'boardedge-frame'];
-    componentInfo['7Yiify8W'] = ['Insight QA', 'https://qa-insight.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
-    componentInfo['3ocB7vZh'] = ['Insight STAGE', 'https://stage-insight.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
+    componentInfo['7Yiify8W'] = ['Insight QA', 'https://qa-insight-o2p.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
+    componentInfo['3ocB7vZh'] = ['Insight STAGE', 'https://stage-o2p-insight.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
     componentInfo['45rOONNT'] = ['Insight DEMO', 'https://demo-insight.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
     componentInfo['3ZF6HrYu'] = ['Insight PROD', 'https://insight.equilar.com', 'images/logo-insight.svg', 'insight-frame'];
     componentInfo['1QNUk4n1'] = ['Survey QA', 'https://qa-survey.equilar.com', 'images/logo-survey.svg', 'insight-frame'];
